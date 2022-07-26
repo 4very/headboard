5e426ae225bdd00023114b1d0fd2b38a597b82b1
Avery Sommer <asommer999@gmail.com>
2022-07-26 10:12:31 -0400

    added launch.json and fixed tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/**/*.{ts,tsx}"],  theme: {
+  content:["./src/**/*.{js,jsx,ts,tsx}"],  theme: {
     extend: {},
   },
   plugins: [],
