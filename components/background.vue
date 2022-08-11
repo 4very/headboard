9efe1c23164dca904f3f621e0d58565f8f5438f5
Avery Sommer <asommer999@gmail.com>
2022-08-11 15:48:09 -0400

    attribution and interactive elements

--- components/background.vue
+++ components/background.vue
@@ -1,10 +1,67 @@
 <script setup>
 
+const attribData = await $fetch('/api/background/metadata');
+
+const liked = ref(false);
+
+const clicked = ref(false);
+
+function clickE () {
+  clicked.value = true;
+  setTimeout(() => {
+    clicked.value = false;
+  }, 1500);
+}
+
 </script>
 
 <template>
   <div class="bg-cover bg-center bg-no-repeat min-h-screen min-w-screen dark:bg-blue3" :style="{backgroundImage: `url('/background/main.jpeg')`}">
     <slot />
+    <div class="flex font-sans font-extralight w-full p-5 bottom-0 left-0 gap-3 absolute justify-items-start">
+      <div class="flex flex-col mt-2 items-center">
+        <button href="#" @click="clickE">
+          <svg
+            class="h-5  text-slate-300 w-5"
+            :class="{'animate-spin':clicked}"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+          </svg>
+        </button>
+        <button href="#" @click="liked=!liked">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 mt-1 stroke-2 text-red-400 w-6 "
+            :class="{'fill-red-400': liked}"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-col">
+        <h1 class="text-2xl text-slate-50 hover:underline">
+          <a :href="attribData['main']['link']">{{ attribData['main']['title'] }}</a>
+        </h1>
+        <div class="text-lg text-slate-300 ">
+          <a :href="attribData['main']['artist_link']" class="hover:underline">
+            {{ attribData['main']['creator'] }}
+          </a>
+          <span> | </span>
+          <a :href="attribData['main']['attribution_link']" class="hover:underline">
+            {{ attribData['main']['attribution'] }}
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,11 +70,4 @@
 export default {
   name: 'BackgroundHandler'
 };
-
-// broswer local storage two images + data
-// display first one
-// if refresh or timestamp > time
-// move 2nd to one and display it
-// add create new second one
-// refresh timestamp
 </script>
--- server/api/background/populate.post.ts
+++ server/api/background/populate.post.ts
@@ -1,8 +1,25 @@
 import fs from 'fs';
 import { backupdata, backupimg } from './metadata.get';
+
+interface imageMeta {
+  image: string,
+  creator: string,
+  artist_link: string,
+  link: string,
+  attribution_link: string,
+  title: string,
+  source: string,
+  attribution: string,
+  liked: true
+}
+
 export default defineEventHandler(async () => {
-  const ancSelection = (await $fetch('https://www.gstatic.com/culturalinstitute/tabext/imax.json')) as {image: string}[];
+  const ancSelection = (await $fetch('https://www.gstatic.com/culturalinstitute/tabext/imax.json')) as imageMeta[];
   const selection = ancSelection[Math.floor(Math.random() * ancSelection.length)];
+
+  selection.link = `https://artsandculture.google.com/${selection.link.replace('asset-viewer/', 'asset/')}`;
+  selection.attribution_link = `https://artsandculture.google.com/${selection.attribution_link.replace('collection/', 'partner/')}`;
+
   fs.writeFileSync(backupdata, JSON.stringify(selection, null, '\t'));
 
   const response: Promise<Blob> = $fetch(`${selection.image}=s1200`, { retry: 5, responseType: 'blob' });
